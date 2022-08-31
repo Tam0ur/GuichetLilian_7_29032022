aca3d413983a4ba7443c00b4f7be559d8b8a4225
Tam0ur <79081889+Tam0ur@users.noreply.github.com>
2022-08-31 18:53:04 +0200

    Add files via upload

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -37,77 +37,37 @@ exports.signup = (req, res, next ) => {
 
 //gestion connexion utilisateur
 exports.login = (req, res, next ) => {
-    const user = {//création nouvel utilisateur avec l'e-mail écrit + mdp hashé
-        email: req.body.email,
-        password : req.body.password
-    };
-    connection.query('SELECT mdp FROM utilisateur', (err, result) => {
-        for ( i = 0; i < result.length; i++){
-            console.log(result[i].mdp)
-            bcrypt.compare(result[i].mdp, user.password)
-            .then(valid => {
-                if (!valid){//gestion si le mdp ne correspond pas
-                    return res.status(401).json({ error : 'Mot de passe incorrect.'});
-                }
-                res.status(200).json({ message : 'Mot de passe correct'});
-            });
-            
-        }
-        return res.status(401).json({ error : 'Mot de passe incorrect.'});
-    })
-        /*bcrypt.compare(req.body.password, user.password)
-        .then(valid => {
-            if (!valid){//gestion si le mdp ne correspond pas
-                return res.status(401).json({ error : 'Mot de passe incorrect.'});
+    bcrypt.hash(req.body.password, 10)//hash du mdp
+    .then(hash => {
+        const user = {//création nouvel utilisateur avec l'e-mail écrit + mdp hashé
+            email: req.body.email,
+            password: hash
+        };
+        connection.query('SELECT * FROM utilisateur WHERE email LIKE ? ', user.email, (err, result_email) => {
+            if ( (result_email[0].email) != user.email ){
+                return res.status(401).json({error : 'Email incorrect.' });
             }
-            res.status(200).json({
-                userId: user._id,
-                token: jwt.sign(
-                    { userId: user._id },
-                    'randompassword',
-                    {expiresIn: '24h' }
-                )
-            });
-        })
-        .catch(error => res.status(500).json({ error }));*/
-};
-
-//CREATE
-/*
-function test() {
-    const  mail = document.getElementById("mail").innerHTML;
-    const mdp = document.getElementById("mdp").innerHTML;
-    const user = { mail, mdp};
-    console.log( user );
-  }
-  
-  con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-    var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
-    con.query(sql, function (err, result) {
-      if (err) throw err;
-      console.log("1 record inserted");
+            else {
+                connection.query('SELECT * FROM utilisateur WHERE mdp LIKE ? ', user.password, (err, result_mdp) => {
+                    if ( (result_mdp[0].mdp) != user.password ){
+                        return res.status(401).json({error : 'Mot de passe incorrect.' });
+                    }
+                    else {
+                        return res.status(401).json({error : 'mdp + email corrects'});
+                    }
+                })
+            }
+        });
+    })
+    .catch(error => {
+        return res.status(500).json({error})
     });
-  });
-  
+};
+/*
   //UPDATE
   connection.query(
-      'UPDATE utilisateurs SET city = ? Where ID = ?',
-      ['John Doe', 1],
-      (err, result) => {
-        if (err) throw err;
-    
-        console.log(`Changed ${result.changedRows} row(s)`);
-      }
-    );
+'UPDATE utilisateurs SET city = ? Where ID = ?',['John Doe', 1]
   
   //DESTROY
-  connection.query(
-      'DELETE FROM utilisateurs WHERE id = ?', [5], (err, result) => {
-        if (err) throw err;
-    
-        console.log(`Deleted ${result.affectedRows} row(s)`);
-      }
-    );
-    */
+'DELETE FROM utilisateurs WHERE id = ?', [5], (err, result) => {
+*/

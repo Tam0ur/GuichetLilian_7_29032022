b01d069568d2f248feda67b319152817b3821749
Tam0ur <79081889+Tam0ur@users.noreply.github.com>
2022-12-06 11:52:11 +0100

    update

---
+++ backend/controller/post.js
@@ -0,0 +1,14 @@
+const connection = require('../utils/db')
+
+exports.createPost = (req, res, next) => {
+    var userId = res.locals.userId
+    let date = new Date().toLocaleDateString("fr_FR");
+    connection.query('INSERT INTO postes (texte, utilisateur, image, date_Création) VALUES (?, ?, ?, ?)', [text, userId,`${req.protocol}://${req.get('host')}/images/${req.file.filename}`, date]), (err, result) => {
+        if (!err){
+            return res.status(201).json({message : 'Texte, utilisateur et date insérés.' });
+        }
+        else {
+            return res.status(500).json({ error : 'Problème création poste.'})
+        }
+    };
+};
---
+++ backend/middleware/multer-config.js
@@ -0,0 +1,22 @@
+const multer = require('multer');
+
+const MIME_TYPES = {//types de fichiers d'images
+    'images.jpg': 'jpg',
+    'images/jpeg': 'jpeg',
+    'images/png': 'png',
+}
+
+const storage = multer.diskStorage({
+    destination: (req, file, callback) => {//enregistrer les fichiers dans le dossier 'images'
+        callback(null, 'images')
+    },
+    filename: (req, file, callback) => {
+        //utiliser le nom do'irigine, remplacer les espace par des underscores + ajout date
+        const name = file.originalname.split(' ').join('_');
+        const extension = MIME_TYPES[file.mimetype];
+        callback(null, name + Date.now() + '.' + extension);
+    }
+});
+
+module.exports = multer({storage}).single('image');
+//exportation du multer, avec la constante storage + gestions unique des téléchargement de fichiers image

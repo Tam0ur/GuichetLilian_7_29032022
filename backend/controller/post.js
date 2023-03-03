fd387f9159c39c1210c9cb2ae127cdda9a3be385
Tam0ur <79081889+Tam0ur@users.noreply.github.com>
2023-03-03 12:19:16 +0100

    update

--- backend/controller/post.js
+++ backend/controller/post.js
@@ -5,25 +5,29 @@ exports.createPost = (req, res, next) => {
     var userId = res.locals.userId
     var text = req.body.texte
     let date = new Date().toLocaleDateString("fr-FR");
-    connection.query('INSERT INTO postes (texte, utilisateur, image, date_Creation) VALUES (?, ?, ?, ?)', [text, userId,`${req.protocol}://${req.get('host')}/images/${req.file.filename}`, date]), (err, result) => {
+    connection.query('INSERT INTO postes (texte, utilisateur, image, date_Creation) VALUES (?, ?, ?, ?)', 
+    [text, userId,`${req.protocol}://${req.get('host')}/images/${req.file.filename}`, date], 
+    (err, result) => {
         if (!err){
+            console.log('réussi')
             return res.status(201).json({message : 'Texte, utilisateur et date insérés.' });
         }
         else {
+            console.log('non')
             return res.status(500).json({ error : 'Problème création poste.'})
         }
-    };
+    });
 };
 
 exports.getAllPosts = (req, res, next) => {
     
-    connection.query('SELECT * FROM postes '), (err, result) => {
+    connection.query('SELECT * FROM postes ', (err, result) => {
         if ( !err ){
-            console.log('1')
+
             res.status(200).json({result});
         }
         else {
             res.status(500).json({ error : 'Problème récupération poste.'})
         }
-    }
+    })
 }

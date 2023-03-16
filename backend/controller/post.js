08092e642973cc0e2287961878c4bbfb5dd575d4
Tam0ur <79081889+Tam0ur@users.noreply.github.com>
2023-03-16 21:07:03 +0100

    update

--- backend/controller/post.js
+++ backend/controller/post.js
@@ -1,14 +1,18 @@
 const connection = require('../utils/db')
+const moment = require('moment')
 
 exports.createPost = (req, res, next) => {
     console.log('1')
     var userId = res.locals.userId
     var text = req.body.texte
-    let date = new Date().toLocaleDateString("fr-FR");
+
+    const now = moment();
+    const date = now.toDate();
+    const  formattedDate =  date().toLocaleDateString('fr-FR', { day: 'numeric', month: 'numeric', year: 'numeric', });
     connection.query('INSERT INTO postes (texte, utilisateur, image, date_Creation) VALUES (?, ?, ?, ?)', 
-    [text, userId,`${req.protocol}://${req.get('host')}/images/${req.file.filename}`, date], 
-    (err, result) => {
-        if (!err){
+    [text, userId,`${req.protocol}://${req.get('host')}/images/${req.file.filename}`, formattedDate], 
+    (error, result) => {
+        if (!error){
             console.log('réussi')
             return res.status(201).json({message : 'Texte, utilisateur et date insérés.' });
         }
@@ -20,14 +24,63 @@ exports.createPost = (req, res, next) => {
 };
 
 exports.getAllPosts = (req, res, next) => {
-    
-    connection.query('SELECT * FROM postes ', (err, result) => {
-        if ( !err ){
-
+    connection.query('SELECT * FROM postes ', (error, result) => {
+        if ( !error ){
             res.status(200).json({result});
         }
         else {
             res.status(500).json({ error : 'Problème récupération poste.'})
+        }
+    })
+}
+
+exports.getThisPost = (req, res, next) => {
+
+    const postId = req.params.id;
+
+    connection.query('SELECT * FROM postes WHERE id=?', 
+    [postId],
+    (error, result) => {
+        if ( !error ){
+            res.status(200).json({result});
+        }
+        else {
+            res.status(500).json({ error : 'Aucun poste correspondant à cet Id'})
+        }
+    })
+}
+
+exports.updatePost = (req, res, next) => {
+
+    const text = req.body.texte
+    const now = moment();
+    const date = now.toDate();
+    const  formattedDate =  date().toLocaleDateString('fr-FR', { day: 'numeric', month: 'numeric', year: 'numeric', });
+
+    connection.query('UPDATE postes SET texte = ?, date_Modification = ?, WHERE id = ?',
+    [text, formattedDate, req.params.id],
+    (error, result) => {
+        if ( !error ){
+            res.status(200).json({result});
+        }
+        else {
+            res.status(500).json({ error : 'Problème mise à jour poste.'})
+        } 
+    });
+}
+
+exports.deletePost= (req, res, next) => {
+
+    const postId = req.params.id;
+
+    connection.query('DELETE FROM postes WHERE id=?',
+    [postId],
+    (error, result) => {
+        if ( !error ){
+            res.status(200).json({result});
+        }
+        else {
+            res.status(500).json({ error : 'Problème suppression poste.'})
         }
     })
 }
--- backend/routes/post.js
+++ backend/routes/post.js
@@ -11,9 +11,9 @@ const postCtrl = require('../controller/post');
 //gestion chemin fonctions sauce
 router.get('/getAllPosts', postCtrl.getAllPosts);
 router.post('/createPost', auth, multer, postCtrl.createPost);
-/*router.get('/:id', auth, postCtrl.getOneSauce);
-router.put('/:id', auth, multer, postCtrl.modifySauce);
-router.delete('/:id', auth, postCtrl.deleteSauce);
+router.get('/getThisPost/:id', auth, postCtrl.getThisPost);
+router.delete('/deletePost/:id', auth, postCtrl.deletePost);
+/*router.put('/:id', auth, multer, postCtrl.modifySauce);
 router.post('/:id/like', auth, postCtrl.likeSauce);
 */
 module.exports = router;
